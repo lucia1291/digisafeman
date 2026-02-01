fbdb842c3593cba484d40ef38e2281830e6cd53f
lucia1291 <vigoroso.lucia@gmail.com>
2026-02-01 17:17:00 +0100

    up

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "digisafeman-v1";
+const CACHE_NAME = "digisafeman-v2";
 const ASSETS = [
   "/digisafeman/",
   "/digisafeman/index.html",
@@ -23,4 +23,15 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
+});
+
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
 });
