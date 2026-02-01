0a921db699acbe79d3eae428efec811c5aa92ece
lucia1291 <vigoroso.lucia@gmail.com>
2026-02-01 16:46:23 +0100

    upadate

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "digisafeman-v1";
+const ASSETS = [
+  "/digisafeman/",
+  "/digisafeman/index.html",
+  "/digisafeman/manifest.webmanifest",
+
+  "/digisafeman/style/style.css",
+  "/digisafeman/style/fontstyle.css",
+  "/digisafeman/style/hamMenuHidden.css",
+
+  "/digisafeman/js/menu.js",
+  "/digisafeman/js/header.js",
+  "/digisafeman/js/bottomnav.js",
+  "/digisafeman/js/breadcrumb.js",
+
+  "/digisafeman/icons/icon-192.png",
+  "/digisafeman/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
+});
